0576d814cfee75ddab3f0efa2cd13d9b0454d4c9
Andrew precious <lilianprecious753@gmail.com>
2024-05-10 02:34:44 +0100

    first-cloned

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const hambuger = document.querySelector(".hambuger");
 const navLinks = document.querySelector(".nav_links");
+const modalBackground = document.querySelector(".modal-background");
+const closeBtn = document.querySelector(".closeBtn");
 
 hambuger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   hambuger.classList.toggle("active");
+});
+
+window.onload = function () {
+  modalBackground.style.display = "block";
+};
+
+modalBackground.addEventListener("click", (e) => {
+  if (e.target.classList.contains("modal-background")) {
+    modalBackground.style.display = "none";
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  modalBackground.style.display = "none";
 });
